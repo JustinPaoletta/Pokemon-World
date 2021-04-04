ca2c04640320d516e2408dbcc373503714d8dc4b
JustinPaoletta <justinpaoletta@gmail.com>
2021-04-04 17:57:45 -0400

    changed server address

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -9,32 +9,32 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getAllPokemon(user) {
-    return this.http.get(`http://ec2-3-129-195-183.us-east-2.compute.amazonaws.com:8000/allpokemon:${user}`);
+    return this.http.get(`http://ec2-18-219-120-68.us-east-2.compute.amazonaws.com:8000/allpokemon:${user}`);
   }
 
   catchOnePokemon(serial, user) {
-    return this.http.post('http://ec2-3-129-195-183.us-east-2.compute.amazonaws.com:8000/catch', {serial: serial, user: user});
+    return this.http.post('http://ec2-18-219-120-68.us-east-2.compute.amazonaws.com:8000/catch', {serial: serial, user: user});
   }
 
   getCaughtPokemon(user) {
-    return this.http.get(`http://ec2-3-129-195-183.us-east-2.compute.amazonaws.com:8000/pokemon-boxes:${user}`);
+    return this.http.get(`http://ec2-18-219-120-68.us-east-2.compute.amazonaws.com:8000/pokemon-boxes:${user}`);
   }
 
   getWildPokemon(user) {
-    return this.http.get(`http://ec2-3-129-195-183.us-east-2.compute.amazonaws.com:8000/allWildPokemon:${user}`);
+    return this.http.get(`http://ec2-18-219-120-68.us-east-2.compute.amazonaws.com:8000/allWildPokemon:${user}`);
   }
 
   isUserNew(user) {
-    return this.http.post('http://ec2-3-129-195-183.us-east-2.compute.amazonaws.com:8000/isUserNew', {user: user});
+    return this.http.post('http://ec2-18-219-120-68.us-east-2.compute.amazonaws.com:8000/isUserNew', {user: user});
   }
 
   changeName(user, pokemon, newName) {
-    return this.http.put('http://ec2-3-129-195-183.us-east-2.compute.amazonaws.com:8000/changeName', {user: user, pokemon: pokemon, newName: newName})
+    return this.http.put('http://ec2-18-219-120-68.us-east-2.compute.amazonaws.com:8000/changeName', {user: user, pokemon: pokemon, newName: newName})
   }
 
   releasePokemon(user, pokemon) {
     const name = pokemon.NAME;
-    return this.http.delete(`http://ec2-3-129-195-183.us-east-2.compute.amazonaws.com:8000/releasePokemon/:${user}/:${name}`)
+    return this.http.delete(`http://ec2-18-219-120-68.us-east-2.compute.amazonaws.com:8000/releasePokemon/:${user}/:${name}`)
   }
 
 }
